909791d5e3878e5ae1694679ded7e1d51103020b
Artur matos <67583930+matos85@users.noreply.github.com>
2020-12-01 15:50:58 +0300

    Update leson2_work2.sql

--- leson2_work2.sql
+++ leson2_work2.sql
@@ -2,6 +2,6 @@
 mysqldump -u root -p examlpe > C:\1\examlpes.sql # создаем дамп
 # ввод пароля
 
-
+# база данных sample должна быть преворительно создана
 
 mysqldump -u root -p sample < C:\1\examlpes.sql #разворачиваем в sample
