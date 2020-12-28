da6ff77ac93f7cb43f37642dd9725e825473740c
Artur Matos <67583930+matos85@users.noreply.github.com>
2020-12-28 13:17:42 +0300

    Add files via upload

---
+++ lesson9.sql
@@ -0,0 +1,90 @@
+-- В базе данных shop и sample присутствуют одни и те же таблицы, учебной базы данных.
+-- Переместите запись id = 1 из таблицы shop.users в таблицу sample.users. Используйте транзакции.
+
+START transaction;
+INSERT INTO sample.users
+SELECT * FROM shop.users WHERE id = 1;
+DELETE FROM shop.users WHERE id = 1;
+-- ROLLBACK; 
+COMMIT;
+
+SELECT * from shop.users;
+SELECT * from sample.users;
+
+
+
+
+--  Создайте двух пользователей которые имеют доступ к базе данных shop.
+-- Первому пользователю shop_read должны быть доступны только запросы на чтение данных,
+-- второму пользователю shop — любые операции в пределах базы данных shop.
+
+
+DROP USER IF EXISTS 'shop_reader'@'localhost';
+CREATE USER 'shop_reader'@'localhost' IDENTIFIED WITH sha256_password BY '123';
+GRANT SELECT ON shop_online.* TO 'shop_reader'@'localhost';
+
+INSERT INTO catalogs(name)
+VALUES('New catalog');
+SELECT * FROM catalogs;
+
+
+DROP USER IF EXISTS 'shop'@'localhost';
+CREATE USER 'shop'@'localhost' IDENTIFIED WITH sha256_password BY '123';
+GRANT ALL ON shop_online.* TO 'shop'@'localhost';
+GRANT GRANT OPTION ON shop_online.* TO 'shop'@'localhost';
+
+
+INSERT INTO catalogs(name)
+VALUES('New catalog');
+ -- have new catalog
+SELECT * FROM catalogs;
+
+
+
+-- Создайте хранимую функцию hello(), которая будет возвращать приветствие,
+-- в зависимости от текущего времени суток.
+-- С 6:00 до 12:00 функция должна возвращать фразу "Доброе утро",
+-- с 12:00 до 18:00 функция должна возвращать фразу "Добрый день",
+-- с 18:00 до 00:00 — "Добрый вечер", с 00:00 до 6:00 — "Доброй ночи".
+
+
+DROP PROCEDURE IF EXISTS hello;
+delimiter //
+CREATE PROCEDURE hello()
+BEGIN
+	CASE 
+		WHEN CURTIME() BETWEEN '06:00:00' AND '12:00:00' THEN
+			SELECT 'Доброе утро';
+		WHEN CURTIME() BETWEEN '12:00:00' AND '18:00:00' THEN
+			SELECT 'Добрый день';
+		WHEN CURTIME() BETWEEN '18:00:00' AND '00:00:00' THEN
+			SELECT 'Добрый вечер';
+		ELSE
+			SELECT 'Доброй ночи';
+	END CASE;
+END //
+delimiter ;
+
+CALL hello();
+
+-- ex 01 с использ. IF ELSE
+DROP PROCEDURE IF EXISTS hello;
+delimiter //
+CREATE PROCEDURE hello()
+BEGIN
+	IF(CURTIME() BETWEEN '06:00:00' AND '12:00:00') THEN
+		SELECT 'Доброе утро';
+	ELSEIF(CURTIME() BETWEEN '12:00:00' AND '18:00:00') THEN
+		SELECT 'Добрый день';
+	ELSEIF(CURTIME() BETWEEN '18:00:00' AND '00:00:00') THEN
+		SELECT 'Добрый вечер';
+	ELSE
+		SELECT 'Доброй ночи';
+	END IF;
+END //
+delimiter ;
+
+CALL hello();
+
+
+--
